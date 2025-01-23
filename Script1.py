c8374160f8667d4197ab9ff0aeb69d227a01256c
srchatte <srinjoyc2023@gmail.com>
2025-01-22 19:05:36 -0500

    Modified both script files.
    - Deleted print from script1.py
    - Added comment to script2.py

--- Script1.py
+++ Script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("Beautiful is better than ugly.")
-print("Explicit is better than  implicit.")
---
+++ Script2.py
@@ -0,0 +1,3 @@
+#Print the lines from Zen of Python.
+print("Simple is better than complex.")
+print("Complex is better than complicated").
